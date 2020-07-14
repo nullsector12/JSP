d0df3df4ae2295427ce003fb975cea26e6348cc2
KimSeungYeon <rlamd0412@gmail.com>
2020-07-14 18:26:02 +0900

    오픈 프로젝트 MVC 모델 수정

---
+++ OpenProjectMVC/src/member/model/Member.java
@@ -0,0 +1,118 @@
+package member.model;
+
+import java.sql.Date;
+
+public class Member {
+	
+	
+	private int idx;
+	private String uid;
+	private String upw;
+	private String uname;
+	private String uphoto;
+	private Date regdate;
+	
+	
+	public Member(int idx, String uid, String upw, String uname, String uphoto, Date regdate) {
+		this.idx = idx;
+		this.uid = uid;
+		this.upw = upw;
+		this.uname = uname;
+		this.uphoto = uphoto;
+		this.regdate = regdate;
+	}
+	
+	public Member() {
+		
+	}
+
+	/**
+	 * @return the idx
+	 */
+	public int getIdx() {
+		return idx;
+	}
+
+	/**
+	 * @param idx the idx to set
+	 */
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	/**
+	 * @return the uid
+	 */
+	public String getUid() {
+		return uid;
+	}
+
+	/**
+	 * @param uid the uid to set
+	 */
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	/**
+	 * @return the upw
+	 */
+	public String getUpw() {
+		return upw;
+	}
+
+	/**
+	 * @param upw the upw to set
+	 */
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+
+	/**
+	 * @return the uname
+	 */
+	public String getUname() {
+		return uname;
+	}
+
+	/**
+	 * @param uname the uname to set
+	 */
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	/**
+	 * @return the uphoto
+	 */
+	public String getUphoto() {
+		return uphoto;
+	}
+
+	/**
+	 * @param uphoto the uphoto to set
+	 */
+	public void setUphoto(String uphoto) {
+		this.uphoto = uphoto;
+	}
+
+	/**
+	 * @return the regdate
+	 */
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	/**
+	 * @param regdate the regdate to set
+	 */
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	public java.util.Date getToDate () { // 사용할 때 = ${member.toDate}
+		return new java.util.Date(regdate.getTime());
+	}
+	
+	
+}
